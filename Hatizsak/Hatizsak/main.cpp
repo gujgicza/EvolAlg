f0f65fc9520907105b32e317cbb193e30b4d82e8
Gujgiczer Anna <gujgiczer.anna@gmail.com>
2016-09-12 09:51:40 +0200

    Added Hello World!

---
+++ Hatizsak/Hatizsak/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hatizsak
+//
+//  Created by Anna Gujgiczer on 9/12/16.
+//  Copyright © 2016 Anna Gujgiczer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
