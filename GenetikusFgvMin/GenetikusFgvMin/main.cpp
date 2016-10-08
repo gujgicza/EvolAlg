7bbf21728654e6ed23d1942c427f8f96913278a2
Gujgiczer Anna <gujgiczer.anna@gmail.com>
2016-10-08 19:29:13 +0200

    Started HF04.

---
+++ GenetikusFgvMin/GenetikusFgvMin/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GenetikusFgvMin
+//
+//  Created by Anna Gujgiczer on 2016/10/08.
+//  Copyright © 2016 Anna Gujgiczer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
